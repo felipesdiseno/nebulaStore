ff04b92034ee1445f72c8c9d05f0f6a269ef5e1d
felipe sanchez <felipe.sdiseno@gmail.com>
2024-08-28 11:38:24 -0500

    color changed

--- front/src/components/card/index.tsx
+++ front/src/components/card/index.tsx
@@ -43,7 +43,7 @@ function Card({
         <button
           onClick={showDetailsCard}
           className={`bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition duration-300 ${
-            showDetails ? "bg-red-500" : ""
+            showDetails ? "bg-red-400" : ""
           }`}
           style={{
             zIndex: 10,
