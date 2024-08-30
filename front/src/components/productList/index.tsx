0da8183cf56bc3c7c6b7b723d41c1b63eab1496d
felipe sanchez <felipe.sdiseno@gmail.com>
2024-08-30 10:55:28 -0500

    shadow add

--- front/src/components/productList/index.tsx
+++ front/src/components/productList/index.tsx
@@ -6,7 +6,7 @@ import { IProduct } from "@/interfaces/IProduct";
 
 const ProductList: React.FC<IProductsList> = ({ products, onAddToCart }) => {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center shadow-sm">
       <div className="grid grid-cols-3 grid-rows-2 gap-4">
         {products.map((product: IProduct) => (
           <Card key={product.id} product={product} onAddToCart={onAddToCart} />
